e4d7f3a8b3317f6dd62ee4edb8e8fa1a1be7918f
CJ0328 <109237558+CJ0328@users.noreply.github.com>
2022-07-14 01:36:27 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector(".menu");
+const navMenu = document.querySelector(".navigation_bar");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((link) =>
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+
